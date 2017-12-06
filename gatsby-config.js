79d330841f05f09dd0381570f2ce07902bc6b44d
Kepler Sticka-Jones <kepler+git@stickajones.org>
2017-12-06 12:15:18 -0700

    Remove unused gatsby-emotion-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   pathPrefix: `/engl2095-axolotl`,
   plugins: [
-    "gatsby-plugin-emotion",
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`
