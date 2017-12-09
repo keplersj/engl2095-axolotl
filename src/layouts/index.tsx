eacae6c77e815238d116d376f5028e84a3506499
Kepler Sticka-Jones <kepler+git@stickajones.org>
2017-12-08 23:05:31 -0700

    Add banner to GitHub repo

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import GithubCorner from "react-github-corner";
 import Helmet from "react-helmet";
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
@@ -12,6 +13,14 @@ const DefaultLayout = (props: DefaultLayoutProps) => (
   <div>
     <Helmet title="Kepler Sticka-Jones - ENGL 2095 - Axolotl" />
     {props.children()}
+    <GithubCorner
+      href="https://github.com/keplersj/engl2095-axolotl"
+      bannerColor="#fff"
+      octoColor="#000"
+      size={80}
+      direction="right"
+      svgStyle={{ mixBlendMode: "lighten" }}
+    />
   </div>
 );
 
