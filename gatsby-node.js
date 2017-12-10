82a6fe1e352f0ad90c7324239bd25c1a4cd785c0
Kepler Sticka-Jones <kepler+git@stickajones.org>
2017-12-10 13:42:05 -0700

    Migrate to natural.
    This will allow me to make use of predication weights to more dynamically determine color shades

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,23 @@
 exports.modifyWebpackConfig = ({ config, stage }) => {
+  config.merge({
+    resolve: {
+      alias: {
+        fs: "memory-fs"
+      }
+    }
+  });
+
+  config.loader("null", {
+    test: /webworker-threads/,
+    loader: "null-loader"
+  });
+
   if (stage === "build-html") {
     config.loader("null", {
       test: /spectacle/,
       loader: "null-loader"
     });
   }
+
+  return config;
 };
--- src/util/classifier.ts
+++ src/util/classifier.ts
@@ -1,14 +1,29 @@
-import * as bayes from "bayes";
+import { BayesClassifier } from "natural";
 
-export const classifier = bayes();
+type rgbArray = [number, number, number];
 
-classifier.learn("They were not animals", "lightblue");
-classifier.learn("He returned many times", "pink");
-classifier.learn("I could think about him a lot", "pink");
-classifier.learn("I am an axolotl", "pink");
-classifier.learn("being an axolotl", "pink");
-classifier.learn("alien to his human life", "pink");
-classifier.learn("Save us", "pink");
+function pickHex(color1: rgbArray, color2: rgbArray, weight: number) {
+  const w1 = weight;
+  const w2 = 1 - w1;
+  const rgb = [
+    Math.round(color1[0] * w1 + color2[0] * w2),
+    Math.round(color1[1] * w1 + color2[1] * w2),
+    Math.round(color1[2] * w1 + color2[2] * w2)
+  ];
+  return rgb;
+}
+
+const classifier = new BayesClassifier();
+
+classifier.addDocument("They were not animals", "lightblue");
+classifier.addDocument("He returned many times", "pink");
+classifier.addDocument("I could think about him a lot", "pink");
+classifier.addDocument("I am an axolotl", "pink");
+classifier.addDocument("being an axolotl", "pink");
+classifier.addDocument("alien to his human life", "pink");
+classifier.addDocument("Save us", "pink");
+
+classifier.train();
 
 export const colorFromText = (passage: string): string =>
-  classifier.categorize(passage);
+  classifier.classify(passage);
